6813203b2d393fde7368720148d97f9b34bb0fd6
OmorFaruk00 <omorfaruk5020@gmail.com>
2022-11-08 01:19:55 +0600

    Routing basic

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('home',function(){
+    return 'Welcome To Home Page';
+});
+// Redirect Routes
+Route::redirect('/','/home');
+// View Routes
+Route::view('user','user',['name' => "Omor Faruk"]);
+// Route Parameters  
+Route::get('user/{user_id}/post/{post_id}', function($userid,$postid){
+    return "User Id ". $userid . " Post Id ".$postid;
+});
+// Optional Parameters with Regular Expression Constraints
+Route::get('user-name/{name?}/{id?}',function($name='Omor Faruk',$id = 0){
+    return "User Name " .$name. " User Id " .$id;
+})->whereNumber('id')->whereAlpha('name');
+// Named Routes with prefix and group
+Route::group(['prefix' => 'user'], function(){
+    Route::get('/profile', function () {
+        return "User Profile";
+    })->name('profile');
+    // Routing to a Single Controller Method
+    Route::get('list',[UserController::class,'index']);
+
+});
